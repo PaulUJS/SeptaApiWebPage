300e81515ee6eef00aebe534614091180686d87d
Paul Ulerick <p.ulerick17@gmail.com>
2023-02-23 21:50:35 -0500

    figured out how to fix bug where data wasn't displaying properly on the page

--- Models/ApiModel.cs
+++ Models/ApiModel.cs
@@ -2,30 +2,30 @@
 
 namespace BlazorWebApp.Models
 {
-    public class ApiModel
+    public class Bus
     {
-        public string lat { get; set; }
-        public string lng { get; set; }
-        public string label { get; set; }
-        public string trip { get; set; }
-        public string VehicleID { get; set; }
-        public string BlockID { get; set; }
-        public string Direction { get; set; }
-        public string destination { get; set; }
-        public string heading { get; set; }
-        public int late { get; set; }
-        public string next_stop_id { get; set; }
-        public string next_stop_name { get; set; }
-        public int next_stop_sequence { get; set; }
-        public string estimated_seat_availability { get; set; }
-        public string Offset { get; set; }
-        public string Offset_sec { get; set; }
-        public int timestamp { get; set; }
+        public string? lat { get; set; }
+        public string? lng { get; set; }
+        public string? label { get; set; }
+        public string? trip { get; set; }
+        public string? VehicleID { get; set; }
+        public string? BlockID { get; set; }
+        public string? Direction { get; set; }
+        public string? destination { get; set; }
+        public string? heading { get; set; }
+        public int? late { get; set; }
+        public string? next_stop_id { get; set; }
+        public string? next_stop_name { get; set; }
+        public int? next_stop_sequence { get; set; }
+        public string? estimated_seat_availability { get; set; }
+        public string? Offset { get; set; }
+        public string? Offset_sec { get; set; }
+        public int? timestamp { get; set; }
     }
 
     public class RootModel
     {
-        public List<ApiModel> model { get; set; }
+        public List<Bus> bus { get; set; }
 
     }
 }
